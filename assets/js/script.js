9c183951e2d0bcbc2b962ae70f2ef93c367866be
voi <wojtek.wacek.dola@gmail.com>
2023-03-12 15:29:23 +1100

    JS: added the function to render the current time and display it in the header.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+
+
+// Checks if the document is ready and executes the funtion every second to refresh the clock, so the time is up to date.
+$(document).ready(function()
+{
+    function setTime() {
+        var today = dayjs();
+        var timeNow = today.format('MMMM D, YYYY h:mm a');
+        $('#date-time').text(timeNow);
+    }
+
+    setInterval(function()
+    {
+        setTime()
+    }, 1000); 
+})
+
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,6 +33,8 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+  
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
